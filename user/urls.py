774eec27b44ab383b383bb3ecbff2f1fdd189468
aqibabdullahi <abdullahiaqib@gmail.com>
2024-07-16 23:24:51 +0100

    pages added, template inheritance used

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('dashboard/', views.user_dashboard, name="dashboard")
+    path('dashboard/', views.user_dashboard, name="dashboard"),
+    path('pickup/', views.pickup, name="pickup"),
+    path('profile/', views.profile, name="profile"),
+    path('notifications/', views.notifications, name="notifications"),
 ]
--- user/views.py
+++ user/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def user_dashboard(request):
     return render(request, "user_dashboard.html")
+
+def pickup(request):
+    return render(request, 'pickup.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def notifications(request):
+    return render(request, 'notifications.html')
