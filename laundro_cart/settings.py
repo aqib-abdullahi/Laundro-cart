fa0e5b3cca3c3bd3730df5d9ca647ffd512ea88b
aqibabdullahi <abdullahiaqib@gmail.com>
2024-08-12 12:16:48 +0100

    redacted secret key to .env file

--- laundro_cart/settings.py
+++ laundro_cart/settings.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z2n4%sy=hde2f%1qroe$=5r8-ocd!17pz75)k+@ei_*7f^25hb'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
