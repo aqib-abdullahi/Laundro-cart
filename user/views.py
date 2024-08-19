dd9c15e084d8d8ff7b025beebf339a1f19b67cff
aqibabdullahi <abdullahiaqib@gmail.com>
2024-08-20 00:29:20 +0100

    fixed duplicate messages display on profil e update page

--- user/views.py
+++ user/views.py
@@ -16,21 +16,33 @@ def pickup(request):
     laundry_items = Laundry.get_all_laundry()
     return render(request, 'pickup.html', {'laundry_items': laundry_items})
 
+
 @login_required
 def profile(request):
+    def replace_message(request, level, new_message):
+        storage = messages.get_messages(request)
+        storage.used = False
+        new_storage = []
+        for message in storage:
+            if message.level != level:
+                new_storage.append(message)
+        storage._queued_messages = new_storage
+        messages.add_message(request, level, new_message)
+        return
+    
     user = request.user
     if request.method == 'POST':
         form = ProfileUpdateForm(request.POST, instance=request.user)
         if form.is_valid:
             form.save()
             if not user.address or not user.phone_number:
-                messages.warning(request, 'Please complete your profile!')
-            messages.success(request, 'Profile updated successfully!')
-            return redirect('profile')
+                replace_message(request, messages.WARNING, 'Please complete your profile before requesting pickup!')
+            return render(request, 'profile.html', {'form': form})
     else:
-        if not user.address or not user.phone_number:
-            messages.warning(request, 'Please complete your profile!')
-        form = ProfileUpdateForm(instance=request.user)
+        form = ProfileUpdateForm(instance=request.user)    
+
+    if request.method != 'POST' and (not user.address or not user.phone_number):
+        replace_message(request, messages.WARNING, 'Please complete your profile before requesting pickup!')
     return render(request, 'profile.html', {'form': form})
 
 @login_required
